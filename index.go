4c877d27cbd503733964e074413fa5179c3e8aec
Gustav Westling <hello@gustav.tv>
2016-11-04 20:16:54 +0100

    #17 - Added comments to index.go

--- index.go
+++ index.go
@@ -4,11 +4,16 @@ import (
 	riak "github.com/basho/riak-go-client"
 )
 
+// SecondaryIndexQueryResult is the items sent to the callback function
+// used by KeysInIndex
 type SecondaryIndexQueryResult struct {
 	Key        string
 	IsComplete bool
 }
 
+// KeysInIndex returns all keys in the index indexName that has the value indexValue
+// The values will be returned to the callbak function
+// When all keys have been returned SecondaryIndexQueryResult.IsComplete will be true
 func (c Command) KeysInIndex(indexName, indexValue string, callback func(SecondaryIndexQueryResult)) Command {
 
 	cb := func(res []*riak.SecondaryIndexQueryResult) error {
