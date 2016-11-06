8ae9822f6465db2fb94aa88eea99e09d37238bb9
Gustav Westling <hello@gustav.tv>
2016-11-06 17:28:24 +0100

    #16 - Added tests for consistancy options with Delete()

--- query_consistency_test.go
+++ query_consistency_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestSetWithConsistancyOptions(t *testing.T) {
+func TestSetRawWithConsistancyOptions(t *testing.T) {
 	_, err := Bucket("raw", "default").
 		SetRaw([]byte("Hello Raw")).
 		WithPw(4).Run(con())
@@ -53,5 +53,87 @@ func TestGetWithConsistancyOptions(t *testing.T) {
 
 	if err.Error() != expectedError {
 		t.Error("Unexpected error PW:", err)
+	}
+}
+
+func TestSetWithConsistancyOptions(t *testing.T) {
+	val := struct {
+		A string
+	}{
+		A: "Hello A",
+	}
+
+	_, err := Bucket("testsuitemap", "maps").
+		Set(val).
+		WithPw(4).Run(con())
+
+	expectedError := "ClientError|[Cluster] all retries exhausted and/or no nodes available to execute command|InnerError|RiakError|0|{n_val_violation,3}"
+
+	if err.Error() != expectedError {
+		t.Error("Unexpected error PW:", err)
+	}
+
+	_, err = Bucket("testsuitemap", "maps").
+		Set(val).
+		WithDw(4).Run(con())
+
+	if err.Error() != expectedError {
+		t.Error("Unexpected error DW:", err)
+	}
+
+	_, err = Bucket("testsuitemap", "maps").
+		Set(val).
+		WithW(4).Run(con())
+
+	if err.Error() != expectedError {
+		t.Error("Unexpected error W:", err)
+	}
+}
+
+func TestDeleteWithConsistancyOptions(t *testing.T) {
+	c := con()
+
+	insert, err := Bucket("raw", "default").SetRaw([]byte("Hello Delete")).Run(c)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	expectedError := "ClientError|[Cluster] all retries exhausted and/or no nodes available to execute command|InnerError|RiakError|0|{n_val_violation,3}"
+
+	_, err = Bucket("raw", "default").Delete(insert.Key).WithDw(4).Run(c)
+
+	if err.Error() != expectedError {
+		t.Error(err)
+	}
+
+	_, err = Bucket("raw", "default").Delete(insert.Key).WithPw(4).Run(c)
+
+	if err.Error() != expectedError {
+		t.Error(err)
+	}
+
+	_, err = Bucket("raw", "default").Delete(insert.Key).WithPr(4).Run(c)
+
+	if err.Error() != expectedError {
+		t.Error(err)
+	}
+
+	_, err = Bucket("raw", "default").Delete(insert.Key).WithPw(4).Run(c)
+
+	if err.Error() != expectedError {
+		t.Error(err)
+	}
+
+	_, err = Bucket("raw", "default").Delete(insert.Key).WithR(4).Run(c)
+
+	if err.Error() != expectedError {
+		t.Error(err)
+	}
+
+	_, err = Bucket("raw", "default").Delete(insert.Key).WithW(4).Run(c)
+
+	if err.Error() != expectedError {
+		t.Error(err)
 	}
 }
