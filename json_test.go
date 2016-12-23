c0aafd0a3c885425229ceb4c6c861dbfd71f7493
Gustav Westling <hello@gustav.tv>
2016-12-23 09:38:22 +0100

    #18 - Automatic Int indexes for JSON structs

--- json_test.go
+++ json_test.go
@@ -266,3 +266,55 @@ func TestSetJSONKeyAfterSet(t *testing.T) {
 		t.Error(err)
 	}
 }
+
+func TestJSONIntIndex(t *testing.T) {
+	type testType struct {
+		User string
+		Age  int `goriakindex:"ageint_int"`
+	}
+
+	users := []testType{
+		{"A", 10},
+		{"B", 10},
+		{"C", 10},
+		{"D", 10},
+		{"E", 13},
+		{"F", 13},
+		{"G", 13},
+		{"H", 13},
+	}
+	for _, u := range users {
+		_, err := Bucket("json", "default").
+			SetJSON(u).
+			Run(con())
+
+		if err != nil {
+			t.Error(err)
+			return
+		}
+	}
+
+	foundCount := 0
+
+	cb := func(key SecondaryIndexQueryResult) {
+		if !key.IsComplete {
+			foundCount++
+		}
+	}
+
+	// With limit
+	Bucket("json", "default").Limit(2).KeysInIndex("ageint_int", "10", cb).Run(con())
+
+	if foundCount != 2 {
+		t.Error("Expected 2 results, got: ", foundCount)
+	}
+
+	foundCount = 0
+
+	// Unlimited
+	Bucket("json", "default").KeysInIndex("ageint_int", "10", cb).Run(con())
+
+	if foundCount != 4 {
+		t.Error("Expected 4 results, got: ", foundCount)
+	}
+}
