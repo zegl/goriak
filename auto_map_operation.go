f4967ba636e4b6a7eeaac91e3775756d2467878b
Gustav Westling <hello@gustav.tv>
2017-01-08 13:20:26 +0100

    #33 - Remove unused code in auto_map_operation.go

--- auto_map_operation.go
+++ auto_map_operation.go
@@ -3,34 +3,26 @@ package goriak
 // This code has been extracted from riak-go-client so that we (goriak) can look at the map operations
 // before executing them.
 //
+// Parts of the original code has been removed.
+//
 // https://github.com/basho/riak-go-client/blob/master/crdt_commands.go#L936:L1118
-// Copyright Basho
-// This file is licensed under Apache License 2.0
 
 // MapOperation contains the instructions to send to Riak what updates to the Map you want to complete
 type riakMapOperation struct {
 	incrementCounters map[string]int64
-	removeCounters    map[string]bool
 
 	addToSets      map[string][][]byte
 	removeFromSets map[string][][]byte
-	removeSets     map[string]bool
 
-	registersToSet  map[string][]byte
-	removeRegisters map[string]bool
+	registersToSet map[string][]byte
 
-	flagsToSet  map[string]bool
-	removeFlags map[string]bool
+	flagsToSet map[string]bool
 
-	maps       map[string]*riakMapOperation
-	removeMaps map[string]bool
+	maps map[string]*riakMapOperation
 }
 
 // IncrementCounter increments a child counter CRDT of the map at the specified key
 func (mapOp *riakMapOperation) IncrementCounter(key string, increment int64) *riakMapOperation {
-	if mapOp.removeCounters != nil {
-		delete(mapOp.removeCounters, key)
-	}
 	if mapOp.incrementCounters == nil {
 		mapOp.incrementCounters = make(map[string]int64)
 	}
@@ -38,23 +30,9 @@ func (mapOp *riakMapOperation) IncrementCounter(key string, increment int64) *ri
 	return mapOp
 }
 
-// RemoveCounter removes a child counter CRDT from the map at the specified key
-func (mapOp *riakMapOperation) RemoveCounter(key string) *riakMapOperation {
-	if mapOp.incrementCounters != nil {
-		delete(mapOp.incrementCounters, key)
-	}
-	if mapOp.removeCounters == nil {
-		mapOp.removeCounters = make(map[string]bool)
-	}
-	mapOp.removeCounters[key] = true
-	return mapOp
-}
-
 // AddToSet adds an element to the child set CRDT of the map at the specified key
 func (mapOp *riakMapOperation) AddToSet(key string, value []byte) *riakMapOperation {
-	if mapOp.removeSets != nil {
-		delete(mapOp.removeSets, key)
-	}
+
 	if mapOp.addToSets == nil {
 		mapOp.addToSets = make(map[string][][]byte)
 	}
@@ -64,9 +42,7 @@ func (mapOp *riakMapOperation) AddToSet(key string, value []byte) *riakMapOperat
 
 // RemoveFromSet removes elements from the child set CRDT of the map at the specified key
 func (mapOp *riakMapOperation) RemoveFromSet(key string, value []byte) *riakMapOperation {
-	if mapOp.removeSets != nil {
-		delete(mapOp.removeSets, key)
-	}
+
 	if mapOp.removeFromSets == nil {
 		mapOp.removeFromSets = make(map[string][][]byte)
 	}
@@ -74,26 +50,8 @@ func (mapOp *riakMapOperation) RemoveFromSet(key string, value []byte) *riakMapO
 	return mapOp
 }
 
-// RemoveSet removes the child set CRDT from the map
-func (mapOp *riakMapOperation) RemoveSet(key string) *riakMapOperation {
-	if mapOp.addToSets != nil {
-		delete(mapOp.addToSets, key)
-	}
-	if mapOp.removeFromSets != nil {
-		delete(mapOp.removeFromSets, key)
-	}
-	if mapOp.removeSets == nil {
-		mapOp.removeSets = make(map[string]bool)
-	}
-	mapOp.removeSets[key] = true
-	return mapOp
-}
-
 // SetRegister sets a register CRDT on the map with the provided value
 func (mapOp *riakMapOperation) SetRegister(key string, value []byte) *riakMapOperation {
-	if mapOp.removeRegisters != nil {
-		delete(mapOp.removeRegisters, key)
-	}
 	if mapOp.registersToSet == nil {
 		mapOp.registersToSet = make(map[string][]byte)
 	}
@@ -101,23 +59,8 @@ func (mapOp *riakMapOperation) SetRegister(key string, value []byte) *riakMapOpe
 	return mapOp
 }
 
-// RemoveRegister removes a register CRDT from the map
-func (mapOp *riakMapOperation) RemoveRegister(key string) *riakMapOperation {
-	if mapOp.registersToSet != nil {
-		delete(mapOp.registersToSet, key)
-	}
-	if mapOp.removeRegisters == nil {
-		mapOp.removeRegisters = make(map[string]bool)
-	}
-	mapOp.removeRegisters[key] = true
-	return mapOp
-}
-
 // SetFlag sets a flag CRDT on the map
 func (mapOp *riakMapOperation) SetFlag(key string, value bool) *riakMapOperation {
-	if mapOp.removeFlags != nil {
-		delete(mapOp.removeFlags, key)
-	}
 	if mapOp.flagsToSet == nil {
 		mapOp.flagsToSet = make(map[string]bool)
 	}
@@ -125,23 +68,8 @@ func (mapOp *riakMapOperation) SetFlag(key string, value bool) *riakMapOperation
 	return mapOp
 }
 
-// RemoveFlag removes a flag CRDT from the map
-func (mapOp *riakMapOperation) RemoveFlag(key string) *riakMapOperation {
-	if mapOp.flagsToSet != nil {
-		delete(mapOp.flagsToSet, key)
-	}
-	if mapOp.removeFlags == nil {
-		mapOp.removeFlags = make(map[string]bool)
-	}
-	mapOp.removeFlags[key] = true
-	return mapOp
-}
-
 // Map returns a nested map operation for manipulation
 func (mapOp *riakMapOperation) Map(key string) *riakMapOperation {
-	if mapOp.removeMaps != nil {
-		delete(mapOp.removeMaps, key)
-	}
 	if mapOp.maps == nil {
 		mapOp.maps = make(map[string]*riakMapOperation)
 	}
@@ -156,18 +84,6 @@ func (mapOp *riakMapOperation) Map(key string) *riakMapOperation {
 	return innerMapOp
 }
 
-// RemoveMap removes a nested map from the map
-func (mapOp *riakMapOperation) RemoveMap(key string) *riakMapOperation {
-	if mapOp.maps != nil {
-		delete(mapOp.maps, key)
-	}
-	if mapOp.removeMaps == nil {
-		mapOp.removeMaps = make(map[string]bool)
-	}
-	mapOp.removeMaps[key] = true
-	return mapOp
-}
-
 func (mapOp *riakMapOperation) hasRemoves(includeRemoveFromSets bool) bool {
 	nestedHaveRemoves := false
 	for _, m := range mapOp.maps {
@@ -177,12 +93,7 @@ func (mapOp *riakMapOperation) hasRemoves(includeRemoveFromSets bool) bool {
 		}
 	}
 
-	rv := nestedHaveRemoves ||
-		len(mapOp.removeCounters) > 0 ||
-		len(mapOp.removeSets) > 0 ||
-		len(mapOp.removeRegisters) > 0 ||
-		len(mapOp.removeFlags) > 0 ||
-		len(mapOp.removeMaps) > 0
+	rv := nestedHaveRemoves
 
 	if includeRemoveFromSets {
 		rv = rv || len(mapOp.removeFromSets) > 0
